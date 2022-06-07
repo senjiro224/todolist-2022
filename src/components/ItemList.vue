71a761fcaf3a89c9eb12df2b004853a796159fe0
senjiro224 <gantaiyeze1@gmail.com>
2022-06-07 17:09:53 +0900

    iroiro_tsukutta

---
+++ src/components/ItemList.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from "vue";
+
+const items = ref([
+  { name: "たまご", price: 100 },
+  { name: "りんご", price: 160 },
+]);
+const newItemName = ref("");
+const newItemPrice = ref(0);
+
+const addItem = () => {
+  items.value.push({ name: newItemName.value, price: newItemPrice.value });
+};
+</script>
+
+<template>
+  <div>
+    <div>ItemList</div>
+    <div v-for="item in items" :key="item.name">
+      <div class="item">
+        <div class="name">名前: {{ item.name }}</div>
+        <div class="price">{{ item.price }} 円</div>
+      </div>
+    </div>
+    <div>
+      <label>
+        名前
+        <input v-model="newItemName" type="text" />
+      </label>
+      <label>
+        価格
+        <input v-model="newItemPrice" type="number" />
+      </label>
+      <button @click="addItem">add</button>
+    </div>
+  </div>
+</template>
+
+<style></style>
